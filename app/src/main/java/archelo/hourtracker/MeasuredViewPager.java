8bf3a0738026814df1aef748ab0cea71f3c49c35
Luis Echegorri <lme4@njit.edu>
2017-10-28 22:43:50 -0400

    Need to implement cross fragment communication

---
+++ app/src/main/java/archelo/hourtracker/MeasuredViewPager.java
@@ -0,0 +1,36 @@
+package archelo.hourtracker;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Archelo on 10/28/2017.
+ */
+
+public class MeasuredViewPager extends ViewPager {
+
+    public MeasuredViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public MeasuredViewPager(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        int height = 0;
+        for(int i = 0; i < getChildCount(); i++) {
+            View child = getChildAt(i);
+            child.measure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED));
+            int h = child.getMeasuredHeight();
+            if(h > height) height = h;
+        }
+
+        heightMeasureSpec = MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+}
--- app/src/main/java/archelo/hourtracker/TabFragment.java
+++ app/src/main/java/archelo/hourtracker/TabFragment.java
@@ -21,22 +21,6 @@ import java.util.Locale;
  */
 
 public class TabFragment extends Fragment {
-    private final static String[] hour = new String[]{"01","02","03","04","05","06","07","08","09","10","11","12"};
-    private final static String[] minutes = new String[]{"00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59"};
-    private final static String[] ampmData = new String[]{"AM","PM"};
-    private final static int STARTPICKER = 0;
-    private final static int ENDPICKER = 1;
-    private final static BigDecimal wage = new BigDecimal(30);
-    private TextView startTime;
-    private TextView endTime;
-    private TextView hoursWorked;
-    private TextView moneyEarned;
-    private NumberPicker hourPicker;
-    private NumberPicker minutePicker;
-    private NumberPicker ampmPicker;
-    private NumberPicker hourPickerTwo;
-    private NumberPicker minutePickerTwo;
-    private NumberPicker ampmPickerTwo;
 
     //TODO checkout textswitcher to see how it looks.
     @Override
